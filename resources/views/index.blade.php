71e624f56660829260c3404bb68c1aa7cec6780d
Laha-hub <81339797 +Laha-hub@users.noreply.github.com>
2021-12-09 22:50:50 +0900

    #19 ルーティングに名前をつける

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>My BBS</title>
-    {{-- <link rel="stylesheet" href="css/style.css"> --}}
     <link rel="stylesheet" href="{{ url('css/style.css') }}">
 </head>
 <body>
@@ -14,7 +13,8 @@
         <ul>
             @forelse ($posts as $index => $post)
                 <li>
-                    <a href="/posts/{{ $index }}">
+                    {{-- <a href="/posts/{{ $index }}"> --}}
+                    <a href="{{ route('posts.show', $index) }}">
                         {{ $post }}
                     </a>
                 </li>
